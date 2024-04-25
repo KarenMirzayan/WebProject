6a86e461bed0bdb50305f5b267266d2ebdbe4b0e
aisana <a_ondassyn@kbtu.kz>
2024-04-25 08:31:46 +0500

    added brand name in details

--- frontend/src/app/product-details/product-details.component.ts
+++ frontend/src/app/product-details/product-details.component.ts
@@ -2,27 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Product } from '../models';
-import {CartService} from "../cart.service";
-import {WishlistService} from "../wishlist.service";
+import { Brand, Product } from '../models';
+import { CartService } from '../cart.service';
+import { WishlistService } from '../wishlist.service';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css',
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
   product!: Product;
   productId!: number;
+  brandName!: string;
 
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService,
     private router: Router,
     private cartService: CartService,
-    private wishlistService: WishlistService,
+    private wishlistService: WishlistService
   ) {}
 
   ngOnInit(): void {
@@ -37,16 +38,21 @@ export class ProductDetailsComponent implements OnInit {
   getProductDetails(id: number) {
     this.apiService.getProduct(id).subscribe((data) => {
       this.product = data;
+      this.getBrandName(this.product.brand);
     });
   }
 
   addToCart(productId: number) {
-    this.cartService.addToCart(productId, 1).subscribe()
+    this.cartService.addToCart(productId, 1).subscribe();
   }
 
   addToWishlist(productId: number) {
-    this.wishlistService.add_item(productId).subscribe()
+    this.wishlistService.add_item(productId).subscribe();
   }
 
-
+  getBrandName(brandId: number) {
+    this.apiService.getBrand(brandId).subscribe((brand: Brand) => {
+      this.brandName = brand.name;
+    });
+  }
 }
